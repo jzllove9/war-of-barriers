7d18328d8bcc7aaa38be076a90aa43a48c6be8b5
jiaziling <jiaziling@kanzhun.com>
2024-03-05 09:25:31 +0800

    chore: 修改打包资源路径

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/war-of-barriers',
     plugins: [
         vue(),
         vueJsx(),
